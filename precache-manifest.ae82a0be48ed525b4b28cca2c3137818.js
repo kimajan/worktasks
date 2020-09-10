09ea1b3230c9f88354ec3e225f153cef5ec30c69
ntek <nativetek1@gmail.com>
2020-09-09 22:09:28 -0700

    Updates

---
+++ precache-manifest.ae82a0be48ed525b4b28cca2c3137818.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30124685b97035e74addaf0f8f4f9f37",
+    "url": "/worktasts/src/index.js/index.html"
+  },
+  {
+    "revision": "0298bb55d7ea427df745",
+    "url": "/worktasts/src/index.js/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5e2612d383f46f1bf53f",
+    "url": "/worktasts/src/index.js/static/css/main.5b1ffff8.chunk.css"
+  },
+  {
+    "revision": "0298bb55d7ea427df745",
+    "url": "/worktasts/src/index.js/static/js/2.0b43844c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/worktasts/src/index.js/static/js/2.0b43844c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2612d383f46f1bf53f",
+    "url": "/worktasts/src/index.js/static/js/main.a5b6f327.chunk.js"
+  },
+  {
+    "revision": "e85ca581d177fba88fea",
+    "url": "/worktasts/src/index.js/static/js/runtime-main.8e1abe21.js"
+  }
+]);
