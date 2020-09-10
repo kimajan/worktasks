ef18bd5fad620e4826acfb54912b8d4baee6fff8
ntek <nativetek1@gmail.com>
2020-09-09 22:16:51 -0700

    Updates

---
+++ precache-manifest.80c3c60d9acee0d1d1faedded728f851.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8df1dfe3c71c7dd6a58b63c8359d39",
+    "url": "/worktasks/index.html"
+  },
+  {
+    "revision": "0298bb55d7ea427df745",
+    "url": "/worktasks/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5e2612d383f46f1bf53f",
+    "url": "/worktasks/static/css/main.5b1ffff8.chunk.css"
+  },
+  {
+    "revision": "0298bb55d7ea427df745",
+    "url": "/worktasks/static/js/2.0b43844c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/worktasks/static/js/2.0b43844c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2612d383f46f1bf53f",
+    "url": "/worktasks/static/js/main.a5b6f327.chunk.js"
+  },
+  {
+    "revision": "6a1b03e7f16da4d0cbc0",
+    "url": "/worktasks/static/js/runtime-main.bc6af8fb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/worktasts/precache-manifest.3655b99963d4742053d8da43ed9e2501.js"
+  "/worktasks/precache-manifest.80c3c60d9acee0d1d1faedded728f851.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/worktasts/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/worktasks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
